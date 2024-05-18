badb9cf65449b8fdcdfa32ec9c904fed9aa65bf6
bhavesh848785 <bhaveshvaghela4545@gmail.com>
2024-05-18 14:58:39 +0530

    php hello world program added

---
+++ 2.3.1_HelloWorld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World Program in php	</title>
+</head>
+<body>
+	We are outside of PHP Tags
+<?php
+		
+	echo "Hello world";
+	echo "Inside PHP Tags";
+
+?>
+</body>
+</html>
