eab0dae85505ebaa62e56a8541f014a888b36196
bhavesh848785 <bhaveshvaghela4545@gmail.com>
2024-05-25 15:38:59 +0530

    Initial commit

---
+++ 2.10.1_If.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>If statement</title>
+</head>
+<body>
+	<?php  
+		$num=12;  
+		if($num<100)
+		{  
+				echo "$num is less than 100";  
+		}  
+
+	?>  
+
+</body>
+</html>
